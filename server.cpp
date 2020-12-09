037869395536be5de0de881116daf38715498293
Igor <igorjovanovic2898@gmail.com>
2020-12-09 15:06:45 +0100

    Ispravka command parsera

--- server.cpp
+++ server.cpp
@@ -45,10 +45,7 @@ std::string Server::CommandParser(std::string str)
         str.erase(0, pos + delimiter.length());
     }
     args.push_back(str);
-    if (args.size() != 3)
-    {
-        return "";
-    }
+
     /*   
      size_t last = 0;
 	size_t next = 0;
